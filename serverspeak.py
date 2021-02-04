8cff3bd56d266fc69dbb91b3a1466c19ab5c925a
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-02-04 13:49:50 -0800

    added main py file

---
+++ serverspeak.py
@@ -0,0 +1,30 @@
+# !git clone https://github.com/RCCRAFT1/serverspeak.git
+# !pip install ngrok
+import docker
+import serverspeak
+import os
+import torch
+import ngrok
+import os
+
+def dockerMain(docker):
+  serverspeak.__init__
+  for docker in docker:
+    int class DockerMain(ServerSpeakDocker)
+
+def core(os, apex):
+  path = '/apex/apex-main.py'
+  if path is None:
+    raise ValueError("No core file is selected.")
+  path(int, core=True)
+
+def tensors(torch, os, tensorboardx):
+  load(path='/TensorboardX/tensors-1.py')
+  torch.cuda(LongTensor, value=3)
+  if cuda.tensor(3) is not None:
+    pass
+    else:
+      return
+ngrok.create_new_tunnel(hostname='SS', ngrok.forward(localhost), creds.ngrok.password=None)
+# !marker apt update install
+# !python3 cuda.py
