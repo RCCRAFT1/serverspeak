9541d5a9f84e86d88e4c67fda530d2062447d565
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-02-14 18:46:18 -0800

    Add files via upload

---
+++ apex/math/MathEnumerable.py
@@ -0,0 +1,43 @@
+import math
+import os
+import torch
+import _amp_index
+
+MathCallDict = dict()
+varMathTracePath
+
+class MathTraceObject(MathTracePath):
+    def classEnum(self, *args, math):
+        math.enumerate(*args, self)
+
+        self._class_amp = None
+        self._class_amp_state = None
+        self._math_class_state = None
+        self._math_class = None
+        self._math_trace_object = None
+        self._https = None
+        self._usechar = None
+        self._char_defs = None
+
+        i.self._math_class
+        for i in self._math_class:
+            usechar.defs(
+                multichar.average()
+                self._class_amp
+            )
+            else:
+                break # Character is not readable
+        if i in self._math_class == math.unreadable:
+            break # Unreadable/unusually character
+
+        r.self._math_class_state
+        for r in self._math_class_state:
+            useavdchar.defs(
+                multiadv.first(value=value)
+                self._class_amp_state
+            )      
+            else:
+                break 
+        await charuser(variac(value=value), device)
+    def run():
+        charuser()    
