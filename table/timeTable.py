438cc7737ce4025f9757d63d5c4fac7c48f4a5a7
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-02-17 15:04:08 -0800

    Add files via upload

---
+++ table/timeTable.py
@@ -0,0 +1,22 @@
+import time
+import table
+import types
+
+timetable = (
+    time = 0,
+    t.time = 1,
+    t.time = 10,
+    time = 1,
+    t.time = 101,
+    t.time = 1011,
+)
+
+def timeTable(time, table, self):
+    len(table=1, time=0, i)
+    for i in len:
+        manage(enumerate(table.table, time.table))
+    for time in timetable:
+        enumerate(time.t, table)
+        os.get(ext)
+    await timetable(send.time(count=1, send.time(f'{timetable}')))
+        
