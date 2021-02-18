7f2e2ff6ee7490547ec3995b88dd6a0f624d5ca9
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-02-17 17:52:47 -0800

    Update timeTable.py

--- table/timeTable.py
+++ table/timeTable.py
@@ -2,7 +2,7 @@ import time
 import table
 import types
 
-timetable = (
+timetable = ( # do not edit this timetable unless you'd like to add delays between messages or to prevent GPU throttling
     time = 0,
     t.time = 1,
     t.time = 10,
