36abef0c7bbc3f4939b8ab6cff723ec5db58d4ac
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-02-19 22:19:31 -0800

    Add files via upload

---
+++ apex/algorithim/EmbedClass.py
@@ -0,0 +1,47 @@
+import embedclass
+import os
+import sys
+import numpy as np
+import math
+import torch
+
+class EmbedClassUse(ClassObjectMethod):
+    def selfint(self, os, *args):
+        int(x=classmethod(), dtype=int())
+        intclass = classmethod(self._dtype)
+
+        self._intclass
+        self._classVector
+        self._ClassDefs
+        self._numpy
+
+        np.mean(self, axis=0 or self._classVector, device)
+        np.sum(self, *args or self._classVector)
+    
+    def start(self, torch):
+        if self._classVector == torch.nn(np.mean):
+            reduce(mean, self) + torch
+        
+        len(start)
+    
+    def stop(self, *args, torch):
+        if self._classVector == torch.nn(np.mean_reduce):
+            reduce(mean, self) + torch.cuda(stop)
+            StopAsyncProcess(self)
+    
+    def loss(loss, torch, int):
+        if torch.loss >> 100:
+            StopAsyncProcess(self)
+        
+        if torch.cuda is None:
+            raise ValueError("No cuda detected")
+
+        loss.compile(np.mean(count), loss)
+        loss = loss.compile(np.mean(count.loss), device(loss))
+
+        if l in loss == enumerate(value=value, iterable=None):
+            isinstance(value.np_mean)
+            else:
+                break
+    
+    def 
