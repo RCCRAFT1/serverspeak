0b177412a60cfc5e2ab53308e6341b2c6a371602
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-03-12 10:26:16 -0800

    Add files via upload

---
+++ apex/algorithim/classes/class-AltAccountDetectorTEST.py
@@ -0,0 +1,48 @@
+# Testing a alt account detector using pycheck and minecraftutils
+import pycheck as check
+import minecraftutils as ms
+from minecraftutils import mc
+import sys
+
+
+import time
+import meta
+from meta import User, Meta, Detect, DetectSession
+print(input"Please enter the account name")
+class AltAccountDetector(DetectAlts):
+
+    def suspection(self, meta, check):
+        suspect = mc.player(mc.playerMeta{meta=self}, suspect)
+
+        for suspect in suspect:
+            mc.player(mc.invalid.session)
+            if real_session == None:
+                mc.execute as mc.player(console(f'kick {mc.player}'), message='You are using a cracked account! Please join with a legit account to play!')
+        
+            elif mc.playerMeta(rejoin=True):
+                time.sleep(100)
+                mc.execute as mc.player(console(f'ban {mc.player}'), message='You are using a cracked account! Please join with a legit account to play!')
+    
+    def identification(meta, os, check):
+        identify = mc.client(mc.player), meta=self
+
+        for client in identify:
+            mc.client({mc.player(input)}, value=reset_mc_player_values or None)
+            client == mc.client(vanilla)
+            else:
+                mc.execute as mc.player(console(f'kick {mc.player}'), message='The client you are using is not a valid client')
+        
+        if identify is None:
+            raise ValueError("No identification process found.")
+        
+        await(callback(suspect), mc)
+        if mc.player == suspect:
+            mc.execute as mc.player(console(f'ban {mc.player}'), message='You are using a cracked account! Please join with a legit account to play!')
+        
+    def banning(self, mc_ban, meta):
+
+        if mc.ban == None:
+            pass
+
+        ban_cmd = f'ban {mc.player}'
+        mc.player = mc.player(suspection)
