cde707651e8d9e293acfbdbf29053c5ee416dfa4
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-04-05 15:28:55 -0700

    added event solver

---
+++ apex/algorithim/solve_event.py
@@ -0,0 +1,28 @@
+import events
+import os
+import sys
+
+import pytorch
+import math
+import numpy
+
+
+events = [0,1,3,4,6,8,13,25]
+
+def events(self, events):
+
+    events = events
+    for 0 in events:
+        events(class.events=self._events)
+    for SolveEvents:
+        solve.events(0)
+    
+    if events == 0 and int(x) is None:
+        calc.int()
+        int(x) is xrange or None
+        x = defaultdict(x=0,y=0)
+    await self.range(calc.int)
+    else:
+        nofunc(all)
+    await self.range(calc.int, 0)
+
