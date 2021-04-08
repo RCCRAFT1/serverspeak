fecef3282e8e394edb95ff0db832c500be39f523
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-04-07 22:26:11 -0700

    Add files via upload

---
+++ table/ASE.py
@@ -0,0 +1,62 @@
+# Async Standard Endorser
+# Build 1,2021
+import pycheck
+import os
+import pytorch as torch
+import sys
+import table
+import math
+
+functions = (
+    row
+    column
+    launch
+    zeroLaunch
+    string
+    area
+    table
+    x
+    select
+    perimeter
+    end
+    def
+    value
+    FUNC
+    math
+    problem
+    eval
+)
+for row in functions:
+    table.row()
+for column in functions:
+    table.column()
+for launch in functions:
+    sys.launch()
+for zeroLaunch in functions:
+    sys.zeroLaunch()
+for string in functions:
+    table.string()
+for area in functions:
+    table.area()
+for table in functions:
+    table.table()
+for x in functions:
+    table.x()
+for select in functions:
+    sys.select() or table.select()
+for perimeter in functions:
+    table.perimeter()
+for end in functions:
+    sys.end()
+for def in functions:
+    table.def() or sys.def()
+for value in functions:
+    table.value()
+for FUNC in functions:
+    sys.func() or table.func()
+for math in functions:
+    math.math() or table.math()
+for problem in functions:
+    table.problem() or math.problem()
+for eval in functions:
+    table.eval() or math.eval() or sys.eval()
