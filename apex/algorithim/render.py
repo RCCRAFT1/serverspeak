610d1b219520466ed2709c197b2153c412d0f683
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-05-05 12:31:48 -0700

    Add files via upload

---
+++ apex/algorithim/render.py
@@ -0,0 +1,45 @@
+import render
+import pytorch as torch
+import os
+import sys
+
+import minecraftutils
+from minecraftutils import mc
+import classes
+import header
+
+callable(render, area=None)
+
+class RenderingHeader(HeaderObject):
+
+    async def header(header, object, torch):
+
+        header = render.render(playerData = save_as_json or json)
+        header.data(ErrorSavingJson = 0) + header.render
+
+        render.render(header.playerData(getattr(header.info)), device)
+        
+        self._render_object
+        self._render_member_object
+
+        await render.render(member_object) + self._render_member_object
+    async def renderMain(header, render, torch, os):
+
+        header_for_rendering = self.header, member.render(geometry)
+        geometry(x=0,y=0,window_ambiguous=None)
+
+        render.frap(frap=0)
+        frap = self._frame_render_count
+
+        await frap.render
+        
+        elif frap.failed_count or frap.failed_render:
+            raise FrameError("The frame {0} failed to render!".format)
+    
+    async def preamble(pre, os, torch):
+        
+        preamble = header.header(geometry)
+        for header in preamble:
+            user.getData(classes = preamble)
+        await send.pre(object.Header)
+    
