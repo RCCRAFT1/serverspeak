15d24210049bc63d1781c451628fe60a60808a6c
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-05-06 08:58:58 -0700

    Update classMinecraftUtils-userParser.py

--- apex/minecraftutils/classes-minecraftutils/classMinecraftUtils-userParser.py
+++ apex/minecraftutils/classes-minecraftutils/classMinecraftUtils-userParser.py
@@ -1,1 +1,32 @@
-w
+import minecraftutils
+from minecraftutils import mc
+import os
+import sys
+
+from mc import playerDataUseable
+
+import classesminecraftutils
+
+class userParser(parseUser):
+
+    async def parseUserMain(parse, os, mc):
+        playername.input = f'{mc.player} = os.cycle' #place minecraft username here or do "f'{mc.player} = os.cycle'"
+        parse1 = mc.player(f'{playername.input}')
+        sys.parse(player_specifics=None, systemLogging=True, debug=True)
+        elif sys.parse=Failure:
+            raise ValueError("User parsing failed")
+        else:
+            os.save_to_file('path') #input path here
+        
+    async def parseUserSecondary(parse, os, mc):
+        self._get_player_characteristics + mc.player(f'{UUID}')
+        self._get_player_metadata + mc.player(f'{mc.player_skin_id}')
+        self._get_player
+
+        mc.player_encoding(sys.encode(memberCommand), mc.player)
+        await os.parse(self._get_player)
+    
+    async def closer(close, os):
+        close = os.exit(code=1)
+        if os.failure(parse) == True:
+            exit(close)
