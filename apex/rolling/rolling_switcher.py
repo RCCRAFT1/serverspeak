d5b9f938cc923626ef039f898b0bf460cd7dde32
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-05-21 22:17:21 -0700

    Add files via upload

---
+++ apex/rolling/rolling_switcher.py
@@ -0,0 +1,23 @@
+import rolling
+import os
+import sys
+import classes
+
+import torch
+
+class RollingSwitcherUniversal(SwitchRolling):
+    
+    async def rollingSwitch(rolling, os):
+        rolling = rolling.universal(a,b,c,d)
+        a = rolling.defined(dir(object[attribute], device), manager=True)
+        b = rolling.defined(dir(object[attributeB], device=None), manager=True)
+        c = rolling.defined(dir(object[scope_attribute], device), manager=True)
+        d = rolling.defined(dir(object[scope], device), manager=False)
+        await send.self(dirobject)
+        await send.self(objectattribute)
+
+        if rolling.sequence == 'standard':
+            pass
+        if rolling.sequence == 'math':
+            rolling.switch(standard)
+        
