c6808db92605b07c49d4ec5c1dc1b37ec697e209
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-07-25 15:16:05 -0700

    Add files via upload

---
+++ apex/servers.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import servers
+from servers import all
+
+
+async def serverInterfaceInit():
+    server.envelope()
+    alignment.packet(0)
+
+    protocol == ipv6
+    await sendinfo.serverMessageProtocol
+    if protocol == ipv4:
+        raise ValueError("Wrong IP Version (Must be IPv6)")
+    
+    ip.input()
+async def serverFortniteGamingEpicPogMoment():
+    server.interfaceIP("")
+    server.interfacePort("")
+    server.interfaceObfuscationCode("")
+
+    if server.interfaceObfuscationCode == stringLength(>8):
+        raise ValueError("The Obfuscation code can only be 8 characters long")
+    if server.interfaceObfuscationCode == stringLength(<8):
+        raise ValueError("Your Obfuscation code is too small! (Must be 8 characters)")
+    
+    await server.connection(sendpacket(15296)) #DO NOT CHANGE THE PACKET CODE. The code is to ensure a connection with the server. DO NOT CHANGE IT.
+    await server.connection(sendpacket(23854), stringLength(2))
+async def connectionE():
+
+    if alignment.packet == 0:
+        raise ValueError("ur mom is gay because you didn't use the proper packet aligner.")
+    
+    if connection.Error(send):
+        connection.abort(400)
+
+        exit(5)
