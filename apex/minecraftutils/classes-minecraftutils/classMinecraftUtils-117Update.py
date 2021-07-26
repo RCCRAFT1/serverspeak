d47b06b66b6676ca004668c552bfd2d7ebf400ec
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-07-25 19:10:07 -0700

    updated for 1.17

---
+++ apex/minecraftutils/classes-minecraftutils/classMinecraftUtils-117Update.py
@@ -0,0 +1,16 @@
+from mc import versionsVariableChecker
+from mc import versionsBoolean
+
+import os
+
+class updateBoolean(booleanObject):
+
+    async def trackUpdateTransaction(os, mc, updater):
+        
+        for key.TransmissionUpdater:
+            user.BooleanField(False)
+            versionsBoolean(checker == True)
+
+            await selfUpdater(trackUpdateTransaction, device)
+            await selfUpdater(getTransactionDate(*), wildcard == True)
+        
