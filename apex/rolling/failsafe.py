0dcfa55e725f263738667d02f91817457a893d2e
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-07-28 20:19:25 -0700

    Add files via upload

---
+++ apex/rolling/failsafe.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+from sys import config
+import failsafe
+
+
+async def failsafe(os, failsafe):
+
+    if error.output(rolling) == fatal_error:
+        raise ValueError("The rolling sequence stopped while switching! Failsafe has been activated.")
+        program.expressionType(all)
+        program.runAllType()
+    
+    if error.output(rolling) == crashed:
+        raise ValueError("The rolling sequence crashed while switching! Failsafe has been activated.")
+        program.expressionTypeNull(*)
+        time.sleep(6)
+        program.enableAllType()
---
+++ apex/rolling/standard.py
@@ -0,0 +1,24 @@
+import rolling
+from rolling import standard_rolling
+import os
+import sys
+import numpy
+import urllib
+
+class StandardRolling(SwitchRollinginStandard) :
+    
+    async def standardClassesLoader(os, rolling):
+
+        rolling()
+        for standard in rolling:
+            classify.method(append(0(energy)), device)
+            dependency(get.http/1(url='dependency.cl/get/httpClassify/0orNone'), getUrl)
+        
+        for rolling.failure in rolling:
+            stop(all(), processes=all)
+        
+        if rolling.value > 1:
+            generate(err.code(1,2,3), store.data())
+            Exception(throw(f'Stopped standard rolling due to {err.code}'))
+        
+        
